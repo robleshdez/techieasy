ed76b5cbed942e853f83255060aad0b6e1f7f909
Juank de Gorvet <50590402+gorvet@users.noreply.github.com>
2025-05-07 17:42:01 -0400

    Revert "primer commit"
    This reverts commit e3c25b8239c1d69157c8023ad042ef85795177a7.

--- app/views/templates/termsTemplate.php
+++
@@ -1,9 +0,0 @@
- <!-- app/views/templates/homeTemplate.php -->
-
- <main id="main" class="main">
-     
-      <?php echo $content; ?>
-
- 
-</main><!-- End #main -->
-
--- public/vendor/php-sse/StopSSEException.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-
-class StopSSEException extends \Exception
-{
-}
